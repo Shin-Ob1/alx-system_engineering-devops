6745ab8707209660860a66da582d530625dff2cd
ShinOb1 <obikelv@gmail.com>
2023-10-23 13:15:41 -0700

    debug json todo

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
+                "task": to.get("title"),
+                "completed": to.get("completed"),
                 "username": username
             } for to in todos]}, jsonfile)
