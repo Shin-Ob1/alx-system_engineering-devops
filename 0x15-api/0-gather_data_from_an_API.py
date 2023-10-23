cebbe785073649524e2f51ca57ddea8fcf410626
ShinOb1 <obikelv@gmail.com>
2023-10-23 12:37:08 -0700

    completed task display

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Displays completed to-do list information for a given employee ID."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [to.get("title") for to in todos if to.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(com)) for com in completed]
